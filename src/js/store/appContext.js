900a1ae5a1904d1f8c0587c5ee8e7afcccf15d85
edianibarrola <edianibarrola@gmail.com>
2023-07-25 02:28:51 +0000

    initial commit

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,48 +1,64 @@
 import React, { useState, useEffect } from "react";
-import getState from "./flux.js";
+import getState, {
+  initialState,
+  saveGameState,
+  loadGameState,
+} from "./flux.js";
 
 // Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
-// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
-const injectContext = PassedComponent => {
-	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
-		const [state, setState] = useState(
-			getState({
-				getStore: () => state.store,
-				getActions: () => state.actions,
-				setStore: updatedStore =>
-					setState({
-						store: Object.assign(state.store, updatedStore),
-						actions: { ...state.actions }
-					})
-			})
-		);
+const injectContext = (PassedComponent) => {
+  const StoreWrapper = (props) => {
+    const [state, setState] = useState(
+      getState({
+        getStore: () => state.store,
+        getActions: () => state.actions,
+        setStore: (updatedStore) =>
+          setState({
+            store: Object.assign(state.store, updatedStore),
+            actions: { ...state.actions },
+          }),
+      })
+    );
 
-		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
-			 *
-			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
-			 *
-			 **/
-		}, []);
+    useEffect(() => {
+      // Load the game state from local storage when the component mounts
+      const initialStateFromStorage = loadGameState();
+      if (initialStateFromStorage) {
+        setState(initialStateFromStorage);
+      }
 
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
-		// on the state of this component
-		return (
-			<Context.Provider value={state}>
-				<PassedComponent {...props} />
-			</Context.Provider>
-		);
-	};
-	return StoreWrapper;
+      /**
+       * EDIT THIS!
+       * This function is the equivalent of "window.onLoad", it only runs once on the entire application lifetime.
+       * You should do your ajax requests or fetch API requests here. Do not use setState() to save data in the
+       * store, instead use actions, like this:
+       *
+       * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
+       *
+       **/
+
+      // Save the game state to local storage whenever it changes
+      saveGameState(state);
+
+      // Clean up the effect to avoid memory leaks
+      return () => {
+        saveGameState(state);
+      };
+    }, [state]);
+
+    // The initial value for the context is not null anymore, but the current state of this component,
+    // the context will now have getStore, getActions, and setStore functions available because they were declared
+    // on the state of this component
+    return (
+      <Context.Provider value={state}>
+        <PassedComponent {...props} />
+      </Context.Provider>
+    );
+  };
+
+  return StoreWrapper;
 };
 
 export default injectContext;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,117 @@
+// flux.js
+import React, { useState, useEffect } from "react";
+
+const initialState = {
+  // Player Stats
+  player: {
+    name: "",
+    level: 1,
+    experience: 0,
+    credits: 1000,
+    inventory: [],
+    equipment: {
+      weapon: null,
+      armor: null,
+      artifact: null,
+      gadget: null,
+    },
+    properties: [],
+  },
+
+  // Economy and Items
+  economy: {
+    contrabandItems: [],
+    energyItems: [],
+    artifactItems: [],
+    elementalItems: [],
+    phoenixItems: [],
+    dragonItems: [],
+  },
+
+  // Game Progression and Achievements
+  gameProgress: {
+    quests: [],
+    achievements: [],
+  },
+
+  // Exploration and Space Battles
+  exploration: {
+    currentLocation: "",
+    spaceBattles: [],
+    planetaryExploration: [],
+  },
+
+  // Events and Random Encounters
+  events: {
+    randomEvents: [],
+  },
+};
+
+// Function to save the game state to local storage
+const saveGameState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("gameState", serializedState);
+  } catch (error) {
+    console.error("Error saving game state:", error);
+  }
+};
+
+// Function to load the game state from local storage
+const loadGameState = () => {
+  try {
+    const serializedState = localStorage.getItem("gameState");
+    if (serializedState === null) {
+      return null;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Error loading game state:", error);
+    return null;
+  }
+};
+
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  // Load the initial state from local storage, if available
+  const initialStateFromStorage = loadGameState();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+  return {
+    store: initialStateFromStorage || initialState,
+    actions: {
+      // Use getActions to call a function within a function
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
+         * fetch().then().then(data => setStore({ "foo": data.bar }))
+         */
+      },
+      changeColor: (index, color) => {
+        const store = getStore();
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+        setStore({ demo: demo });
+      },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      // Function to save the game state to local storage
+      saveGameState: () => {
+        const state = getStore();
+        saveGameState(state);
+      },
+
+      // Function to load the game state from local storage
+      loadGameState: () => {
+        const state = loadGameState();
+        if (state) {
+          setStore(state);
+        }
+      },
+    },
+  };
 };
 
 export default getState;
+export { initialState, saveGameState, loadGameState };
